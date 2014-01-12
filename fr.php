10beaf13c56581a05e58d99084994850202772bb
HadrienBoyer <hadrienboyer@gmail.com>
2014-01-12 17:17:40 +0100

    Fichier de traduction.

---
+++ fr.php
@@ -0,0 +1,99 @@
+<?php
+/**
+ *
+ * River Addon French language file
+ * @author Hadrien Boyer
+ *
+ * @package river_addon
+ * 
+ */
+
+$french = array(
+
+	'admin:settings:river_addon'	=> 'River Addon',	
+	'river_addon:albums:none'		=> 'Aucun album créé pour l\'instant.',
+	'river_addon:announcement:title'	=> 'Annonce !',
+	'river_addon:friends:none'		=> "Vous n'avez pas encore ajouté d'amis.",
+	'river_addon:friends:online'	=> 'Amis en ligne',
+	'river_addon:friends:online:none' 	=> "Pas d'amis en ligne",
+	'river_addon:groups:created:none' => "Aucun groupe n'a encore été créé.",
+	'river_addon:groups:join'		=> 'Aller aux groupes.',
+	'river_addon:groups:none'		=> "Vous n'avez pas encore rejoint de groupe.",
+	'river_addon:header:announcement'	=> 'Annonces :',
+	'river_addon:header:appearance'	=> 'Apparence :',
+	'river_addon:header:general'	=> 'Général :',
+	'river_addon:header:sidebar'	=> 'Barre latérale :',
+	'river_addon:info:columns'		=> 'Sélectionnez les zones que vous souhaitez afficher avec une disposition de trois colonnes. Laissez cette case décochée pour une disposition de deux colonnes.',
+	'river_addon:info:announcement'	=> "Décidez où vous souhaitez afficher des annonces. Sur la page d'activité ou l'ensemble du site.",
+	'river_addon:info:modules'		=> '<p>Sélectionnez les modules de la barre latérale, nombre d\'entités à afficher et de glisser-déposer les modules à l\'ordre que vous préférez. Sélectionner "Vos groupes" permet à chaque utilisateur de choisir le nombre de groupes à être affiché !</p>Décidez où afficher les modules : Sur la page d\'activité ou l\'ensemble du site.</p>',
+	'river_addon:info:notenabled'	=> "Cette option a été désactivée par l'administrateur système !",
+	'river_addon:label:announcement' => 'Activer les Annonces',
+	'river_addon:label:columns'		=> 'Diposition en trois colonnes',
+	'river_addon:label:comments' 	=> 'Commentaires',
+	'river_addon:label:groupcount'	=> "Nombre de groupes à afficher sur la page d'activité",
+	'river_addon:label:html'		=> 'Ajouter une Annonce :',
+	'river_addon:label:module' 		=> 'Selectionner le Style du Module',
+	'river_addon:label:show_albums' => 'Derniers Albums photo',
+	'river_addon:label:show_custom'	=> 'Contenu personnalisé',
+	'river_addon:label:show_friends'	=> 'Amis',
+	'river_addon:label:show_friends_online' => 'Amis en Ligne',
+	'river_addon:label:show_groups' => 'Vos Groupes',
+	'river_addon:label:show_icon' 	=> 'Icône du Profil',
+	'river_addon:label:show_latest_groups' 	=> 'Derniers Groupes',
+	'river_addon:label:show_latest_members' => 'Derniers Membres',
+	'river_addon:label:show_menu' 	=> 'Menu propriétaire',
+	'river_addon:label:show_tagcloud' 	=> 'Nuage de Tag',
+	'river_addon:label:show_ticker' => 'Tweet Ticker',
+	'river_addon:label:thewire' 	=> 'Afficher The Wire (le fil) sur la page d\'activité ?',
+	'river_addon:label:taborder'    => "Choisissez l'ordre des Onglets",
+	'river_addon:latest:groups' 	=> 'Derniers Groupes',
+	'river_addon:latest:members' 	=> 'Derniers Membres',
+	'river_addon:option:activity'	=> 'Activité',
+	'river_addon:option:announcement'	=> 'Annonces',
+	'river_addon:option:aside'		=> 'A part',
+	'river_addon:option:default' 	=> 'Tous, Moi, Amis',
+	'river_addon:option:featured'	=> 'En vedette',
+	'river_addon:option:friend' 	=> 'Amis, Moi, Tous',
+	'river_addon:option:info'		=> 'Info',
+	'river_addon:option:mine' 		=> 'Moi, Amis, Tous',
+	'river_addon:option:order'		=> 'Ordre',
+	'river_addon:option:sidebar:left' 	=> 'Barre latérale Gauche',
+	'river_addon:option:sidebar:right' 	=> 'Barre latérale Droite',
+	'river_addon:option:site'		=> 'Site',
+	'river_addon:select:check'		=> 'Tout cocher',
+	'river_addon:select:context'	=> 'Sélectionnez le contexte',
+	'river_addon:select:none'		=> 'Aucune sélection',
+	'river_addon:select:selected'	=> 'sélectionné',
+	'river_addon:select:uncheck'	=> 'Tout décocher',
+	'river_addon:settings:order:ok'	=> 'Le nouvel ordre a été enregistré avec succès.',
+	'river_addon:settings:save:fail'	=> 'Désolé, nous ne pouvons pas enregistrer vos paramètres.',
+	
+	'river_addon:subtype:all' 		=> 'Tous',
+	'river_addon:subtype:blog' 		=> 'Blogs',
+	'river_addon:subtype:bookmarks' => 'Favoris',
+	'river_addon:subtype:file' 		=> 'Fichiers',
+	'river_addon:subtype:page' 		=> 'Pages',
+
+	'river_addon:tab:announcement'	=> 'Annonces',
+	'river_addon:tab:general'		=> 'Général',	
+	'river_addon:tab:sidebar'		=> 'Barre latérale',		
+	'river_addon:thewire'			=> 'Quoi de neuf ?',
+	'river_addon:tooltip:settings'	=> 'Paramètres du River Addon',
+	'river_addon:welcome'			=> 'Bonjour, %s',
+					
+	// sidebar tweets	
+	'river_addon:label:info' 		=> 'Information',
+	'river_addon:tweet:1' 			=> "Vous pouvez modifier ce texte en éditant le fichier de langue du river addon plug-in",
+	'river_addon:tweet:2' 			=> 'L\'administrateur du site peut désactiver cette fonctionnalité sur la page des paramètres du plug-in',
+	'river_addon:tweet:3'			=> "Il s'agit d'un court message de l'administrateur du site",
+	'river_addon:tweet:4' 			=> 'Rejoindre un groupe est aussi simple que de cliquer sur le lien "Rejoindre le groupe" de la page de profil d\'un groupe',
+
+	// demo	
+	'river_addon:demo:text'			=> '<p>Passez tout contenu du fichier de démonstration inclus sur une barre latérale.</p><p>Par exemple Google AdSense.</p>',
+	'river_addon:demo:title'		=> 'Info',
+	
+);
+
+add_translation("fr", $french);
+
+?>
